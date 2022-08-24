be55f01293d07151df82fe2e2b489b20837e0878
Aaron Foster <50079365+pythonpete32@users.noreply.github.com>
2022-08-24 08:37:26 +0400

    update config

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #! /opt/homebrew/bin/zsh
 source ./.env
 
-NATION=''
-veNATION=''
-LPTOKEN=''
+NATION='0x333A4823466879eeF910A04D473505da62142069'
+veNATION='0xF7deF1D2FBDA6B74beE7452fdf7894Da9201065d'
+LPTOKEN='0x6f57329D43f3De9fF39d4424576dB920b55060B3'
 
 forge create --rpc-url $RPC_URL \
     --private-key $PRIVATE_KEY src/BoostedLiquidityDistributor.sol:BoostedLiquidityDistributor \
